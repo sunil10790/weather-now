8f06971f6b43ab114a0a9ac0d3a2d4e2dfd981a5
Sunil Kumar <sunil10790@outlook.com>
2021-09-13 00:26:11 +0530

    Display current weather and temparature on the console.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('postman-request')
 
-const url = "http://api.weatherstack.com/current?access_key=cc87dbaebf12b096a1faabe9847e7d94&query=12.9716,77.5946"
+const url = "http://api.weatherstack.com/current?access_key=cc87dbaebf12b096a1faabe9847e7d94&query=12.9716,77.5946&units=m"
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true}, (error, response) => {
+    const currentData = response.body.current;
+    console.log(currentData.weather_descriptions[0] + '. It is currently ' + currentData.temperature + ' degrees out. It feels like ' + currentData.feelslike + ' degrees out.')
 })
